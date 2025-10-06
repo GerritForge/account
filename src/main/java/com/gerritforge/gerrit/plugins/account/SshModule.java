6d66962bd2c1c52186feb802769d512341aec555
Antonio Barone <syntonyze@gmail.com>
2025-10-06 11:19:16 +0200

    Fix class extension problem with SshModule
    Change [1] breaks inheritance of this plugin's `SshModule`, and
    the code does not compile.
    
    [1]  https://gerrit-review.googlesource.com/c/gerrit/+/400622
    
    Change-Id: I4dfb366017b5b4b526d22d2a0748ed2f6923f52a

--- src/main/java/com/gerritforge/gerrit/plugins/account/SshModule.java
+++ src/main/java/com/gerritforge/gerrit/plugins/account/SshModule.java
@@ -14,9 +14,17 @@
 
 package com.gerritforge.gerrit.plugins.account;
 
+import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.sshd.PluginCommandModule;
+import com.google.inject.Inject;
 
 public class SshModule extends PluginCommandModule {
+
+  @Inject
+  SshModule(@PluginName String pluginName) {
+    super(pluginName);
+  }
+
   @Override
   protected void configureCommands() {
     command(DeleteAccountCommand.class);
